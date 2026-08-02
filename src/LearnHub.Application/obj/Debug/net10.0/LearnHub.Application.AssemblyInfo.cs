2a1ed56c24333da2f8fede1982d56c9b688eae9c
Amr Nabih <amrnabih112@gmail.com>
2026-08-02 13:27:50 +0300

    feat: adding queres of the users

--- src/LearnHub.Application/obj/Debug/net10.0/LearnHub.Application.AssemblyInfo.cs
+++ src/LearnHub.Application/obj/Debug/net10.0/LearnHub.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnHub.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b826a5ac50f50945088ed417eaa67a506fbe2c52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe2a8a5e828f33b9383ddf2c49cd58f51719927")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnHub.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnHub.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

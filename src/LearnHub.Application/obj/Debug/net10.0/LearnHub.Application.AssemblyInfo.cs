0e451b70ba13f08c7117d04567ee30ec8ae397f8
Amr Nabih <amrnabih112@gmail.com>
2026-08-09 07:50:55 +0300

    Remove secrets from configuration

--- src/LearnHub.Application/obj/Debug/net10.0/LearnHub.Application.AssemblyInfo.cs
+++ src/LearnHub.Application/obj/Debug/net10.0/LearnHub.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnHub.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d945254142c246ff8ab542727c16d1eeca46193e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a00bb69083b5a3ddbd722eb9a80d62a0c76ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnHub.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnHub.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
